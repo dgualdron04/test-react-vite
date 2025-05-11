c6fb7d102b3c6b57d5920eaa5c59093b2c4b3b99
Diego Gualdron <dgualdron04@gmail.com>
2025-05-11 12:33:52 -0500

    Application Complete

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <article>
+        <p>Clase DWFS</p>
+      </article>
+      <section>
+        <p>23 Abril</p>
+      </section>
+      <footer>
+        <p>2025</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
